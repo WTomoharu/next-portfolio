68c1cd859aa08358a3cad7438c65550b6fe09a3a
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-09-02 10:45:03 +0900

    update: add public dir to gitignore and create static files dir.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const CopyPlugin = require("copy-webpack-plugin")
+
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
+  trailingSlash: true,
   reactStrictMode: true,
+
+  webpack(config, options) {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, "static-files"),
+            to: path.join(__dirname, "public")
+          },
+        ],
+      }),
+    )
+
+    return config
+  }
 }
