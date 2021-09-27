f8e38adfb92f0308d52179bd3f9965ef772d361b
WTomoharu <WTomoharu@users.noreply.github.com>
2021-09-27 06:20:51 +0000

    deploy: 053595edeae160daa74f8fb41bad1b59050d774b

---
+++ _next/static/7Tn7qjdTpLO7zDfIjAKqc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-96b513aa6d3e3e02b4a6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
